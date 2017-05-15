6fe939bced79dc5d4c13b64141d187b643e80e52
Robert Tang <bayareabird@gmail.com>
2017-05-15 12:42:08 -0700

    changes made

--- server/libs/mongo.js
+++ server/libs/mongo.js
@@ -2,6 +2,6 @@
 var mongo = require('mongoskin');
 
 // Connect to MongoDB
-var db = mongo.db("mongodb://localhost/demo_db");
+var db = mongo.db("mongodb://localhost/workorder");
 module.exports = db;
 
